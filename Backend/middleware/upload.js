8a4069c4e8badebb2e4542252d9e69ce8fa00f04
Kasmi Thapa <kasmithapa00@gmail.com>
2025-03-01 16:34:52 +0545

    updating

--- Backend/middleware/upload.js
+++ Backend/middleware/upload.js
@@ -2,23 +2,20 @@ const multer = require('multer');
 const { GridFsStorage } = require('multer-gridfs-storage');
 require('dotenv').config();
 
-// Setup GridFS storage
 const storage = new GridFsStorage({
-    url: process.env.MONGO_URI, // Ensure this is set in your .env file
+    url: process.env.MONGO_URI,
     file: (request, file) => {
         const match = ["image/png", "image/jpg"];
 
-        // If file type doesn't match, return a unique filename
-        if (!match.includes(file.mimetype)) {
-            return {
-                bucketName: "photos",
-                filename: `${Date.now()}-rent-${file.originalname}`
-            };
+        if (match.indexOf(file.mimetype) === -1) {
+            return `${Date.now()}-rent-${file.originalname}`;
+
         }
 
         return {
             bucketName: "photos",
             filename: `${Date.now()}-rent-${file.originalname}`
+
         };
     }
 });
